37ef632111676778b79477fe85ffc8d3ab2a15e6
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-07 02:55:08 +0600

    Node module systems

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const people = require('./people');
+// console.log(people);
+// console.log(people.people);
+// console.log(people.a);
+// people.test();
+
 const _ = require('lodash');
 
-console.log(_.last([1,2,3]));
+console.log(_.last(people));
+
---
+++ people.js
@@ -0,0 +1,18 @@
+// (function(exports,require,module,__filename,__dirname){ //module wrapper function
+var people = ['sakib','tamim','mashrafi'];
+// var a = 6;
+// function test(){
+//     console.log(test);
+// }
+// module.exports = {
+//     // people: people,
+//     // a: a,
+//     // test: test
+//     people,
+//     a,
+//     test,
+// };
+// return module.exports;
+// });
+
+module.exports = people;
