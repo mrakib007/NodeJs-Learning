85207a484f07342c6c566153ae4e4dfee305f9bb
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-08 11:03:47 +0600

    Read Stream learning

--- Core-Concepts/StreamsBuffer/index.js
+++ Core-Concepts/StreamsBuffer/index.js
@@ -1,7 +1,39 @@
-const fs = require('fs');
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write('<html><head><title>Form</title></head>');
+    res.write('<body><form method="post" action="/process"><input name="message"/></form></body>');
+    res.end();
+  }else if(req.url === '/process' && req.method === 'POST'){
+    //when data is received in a chunk
+    const body = [];
+    req.on('data',(chunk)=>{
+        body.push(chunk);
+    })
 
-const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`,'utf-8');
-ourReadStream.on(`data`,(chunk)=>{
-    console.log(chunk);
-    // console.log(chunk.toString()); //if you dont want to use utf8
-})
+    //when response given after entire data is here
+    req.on('end',()=>{
+        console.log('stream finished');
+        const parsedBody = Buffer.concat(body).toString();
+        console.log(parsedBody);
+        res.write('Thank you for submitting');
+        res.end();
+    })
+    
+  }else{
+    res.write('Not found');
+    res.end();
+  }
+});
+
+server.listen(3000);
+console.log("listening on port 3000");
+
+
+// const fs = require('fs');
+
+// const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`,'utf-8');
+// ourReadStream.on(`data`,(chunk)=>{
+//     console.log(chunk);
+//     // console.log(chunk.toString()); //if you don't want to use utf8
+// })
