64c6838de79f913566632a16ecbe6d6edf062d04
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-08 11:15:46 +0600

    Read Stream learning

---
+++ Core-Concepts/StreamsBuffer/writeStream.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+ourReadStream.on(`data`,(chunk)=>{
+    ourWriteStream.write(chunk);
+    // console.log(chunk.toString()); //if you don't want to use utf8
+})
