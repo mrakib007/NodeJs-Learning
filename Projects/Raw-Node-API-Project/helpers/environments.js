89caaec8f66002e309d4d73d1a782b72eed1c8e3
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-10 00:17:51 +0600

    Solved environment variable running problem

--- Projects/Raw-Node-API-Project/helpers/environments.js
+++ Projects/Raw-Node-API-Project/helpers/environments.js
@@ -5,13 +5,17 @@ environments.staging = {
 };
 environments.production = {
     port : 5000,
-    envName: 'staging',
+    envName: 'production',
 };
-//determine which environment was passed
-const currentEnvironment = typeof(process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV : 'staging';
+// determine which environment was passed
+const currentEnvironment =
+    typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
 
-//export corresponding environment object
-const environmentToExport = typeof(environ[currentEnvironment]) === 'object' ? environ[currentEnvironment] : environments.staging; 
+// export corresponding environment object
+const environmentToExport =
+    typeof environments[currentEnvironment] === 'object'
+        ? environments[currentEnvironment]
+        : environments.staging;
 
 //export module
 module.exports = environmentToExport;
