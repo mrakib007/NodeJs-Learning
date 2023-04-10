f49bf0def6cd8610fbc40e43b4c47eb5bbd11662
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-10 10:56:39 +0600

    Writing in the file functionality added

--- Projects/Raw-Node-API-Project/index.js
+++ Projects/Raw-Node-API-Project/index.js
@@ -4,8 +4,13 @@
 const http = require('http');
 const {handleReqRes} = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const data = require('./lib/data');
 //app object - module scaffolding
 const app = {};
+
+data.create('test','newFile',{'name':'Bangladesh','language':'Bangla'},(error)=>{
+    console.log(`error was`,error);
+})
 //create server
 app.createServer = () =>{
     const server = http.createServer(app.handleReqRes);
