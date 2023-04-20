e84fc1772ce9d5ede4b476ba4d50bc86145fe419
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-04-21 00:45:49 +0600

    get request added

--- Projects/Raw-Node-API-Project/handlers/routeHandlers/userHandler.js
+++ Projects/Raw-Node-API-Project/handlers/routeHandlers/userHandler.js
@@ -1,6 +1,7 @@
 //handler to handle user related route
 const data = require('../../lib/data');
 const {hash} = require('../../helpers/utilities');
+const {parseJSON} = require('../../helpers/utilities');
 
 const handler = {};
 handler.userHandler = (requestProperties, callback) => {
@@ -13,72 +14,162 @@ handler.userHandler = (requestProperties, callback) => {
 };
 handler._users = {};
 
+// handler._users.post = (requestProperties, callback) => {
+//   const firstName =
+//     typeof requestProperties.body.firstName === "string" &&
+//     requestProperties.body.firstName.trim().length > 0
+//       ? requestProperties.body.firstName
+//       : false;
+
+//   const lastName =
+//     typeof requestProperties.body.lastName === "string" &&
+//     requestProperties.body.lastName.trim().length > 0
+//       ? requestProperties.body.lastName
+//       : false;
+
+//   const phone =
+//     typeof requestProperties.body.phone === "string" &&
+//     requestProperties.body.phone.trim().length > 11
+//       ? requestProperties.body.phone
+//       : false;
+
+//   const password =
+//     typeof requestProperties.body.password === "string" &&
+//     requestProperties.body.password.trim().length > 0
+//       ? requestProperties.body.password
+//       : false;
+
+//   const tosAgreement =
+//     typeof requestProperties.body.tosAgreement === "boolean" &&
+//     requestProperties.body.tosAgreement.trim().length > 0
+//       ? requestProperties.body.tosAgreement
+//       : false;
+    
+//     if(firstName && lastName && phone && password && tosAgreement){
+//         //make sure that user doesn't already exist
+//         data.read('users',phone,(error1,)=>{
+//             if(error1){
+//                 let userObject = {
+//                     firstName,
+//                     lastName,
+//                     phone,
+//                     password: hash(password),
+//                     tosAgreement,
+//                 }
+//                 //store the use to db
+//                 data.create('users',phone,userObject,(error2)=>{
+//                     if(!error2){
+//                         callback(200,{
+//                             message: 'User was created successfully',
+//                         })
+//                     }else{
+//                         callback(500,{'error': 'Could not create user'})
+//                     }
+//                 });
+//             }else{
+//                 callback(500,{
+//                     'error': 'There was a problem in server side'
+//                 })
+//             }
+//         });
+//     }else{
+//         callback(400,{
+//             error: 'You have a problem in your request',
+//         })
+//     }
+// };
+
 handler._users.post = (requestProperties, callback) => {
   const firstName =
-    typeof requestProperties.body.firstName === "string" &&
-    requestProperties.body.firstName.trim().length > 0
-      ? requestProperties.body.firstName
-      : false;
+      typeof requestProperties.body.firstName === 'string' &&
+      requestProperties.body.firstName.trim().length > 0
+          ? requestProperties.body.firstName
+          : false;
 
   const lastName =
-    typeof requestProperties.body.lastName === "string" &&
-    requestProperties.body.lastName.trim().length > 0
-      ? requestProperties.body.lastName
-      : false;
+      typeof requestProperties.body.lastName === 'string' &&
+      requestProperties.body.lastName.trim().length > 0
+          ? requestProperties.body.lastName
+          : false;
 
   const phone =
-    typeof requestProperties.body.phone === "string" &&
-    requestProperties.body.phone.trim().length > 11
-      ? requestProperties.body.phone
-      : false;
+      typeof requestProperties.body.phone === 'string' &&
+      requestProperties.body.phone.trim().length === 11
+          ? requestProperties.body.phone
+          : false;
 
   const password =
-    typeof requestProperties.body.password === "string" &&
-    requestProperties.body.password.trim().length > 0
-      ? requestProperties.body.password
-      : false;
+      typeof requestProperties.body.password === 'string' &&
+      requestProperties.body.password.trim().length > 0
+          ? requestProperties.body.password
+          : false;
 
   const tosAgreement =
-    typeof requestProperties.body.tosAgreement === "boolean" &&
-    requestProperties.body.tosAgreement.trim().length > 0
-      ? requestProperties.body.tosAgreement
-      : false;
-    
-    if(firstName && lastName && phone && password && tosAgreement){
-        //make sure that user doesn't already exist
-        data.read('users',phone,(error1,)=>{
-            if(error1){
-                let userObject = {
-                    firstName,
-                    lastName,
-                    phone,
-                    password: hash(password),
-                    tosAgreement,
-                }
-                //store the use to db
-                data.create('users',phone,userObject,(error2)=>{
-                    if(!error2){
-                        callback(200,{
-                            message: 'User was created successfully',
-                        })
-                    }else{
-                        callback(500,{'error': 'Could not create user'})
-                    }
-                });
-            }else{
-                callback(500,{
-                    'error': 'There was a problem in server side'
-                })
-            }
-        });
-    }else{
-        callback(400,{
-            error: 'You have a problem in your request',
-        })
-    }
+      typeof requestProperties.body.tosAgreement === 'boolean' &&
+      requestProperties.body.tosAgreement
+          ? requestProperties.body.tosAgreement
+          : false;
+
+  if (firstName && lastName && phone && password && tosAgreement) {
+      // make sure that the user doesn't already exists
+      data.read('users', phone, (err1) => {
+          if (err1) {
+              const userObject = {
+                  firstName,
+                  lastName,
+                  phone,
+                  password: hash(password),
+                  tosAgreement,
+              };
+              // store the user to db
+              data.create('users', phone, userObject, (err2) => {
+                  if (!err2) {
+                      callback(200, {
+                          message: 'User was created successfully!',
+                      });
+                  } else {
+                      callback(500, { error: 'Could not create user!' });
+                  }
+              });
+          } else {
+              callback(500, {
+                  error: 'There was a problem in server side!',
+              });
+          }
+      });
+  } else {
+      callback(400, {
+          error: 'You have a problem in your request',
+      });
+  }
 };
 
-handler._users.get = (requestProperties, callback) => {};
+handler._users.get = (requestProperties, callback) => {
+  //check the phone number is valid or not
+  const phone =
+    typeof requestProperties.queryStringObject.phone === "string" &&
+    requestProperties.queryStringObject.phone.trim().length > 11
+      ? requestProperties.body.phone
+      : false;
+    if(phone){
+      //lookup the user
+      data.read('users',phone,(err,u)=>{
+        const user = {...parseJSON(u)};
+        if(!err && user){
+          delete user.password;
+          callback(200,user);
+        }else{
+          callback(404,{
+            'error': 'Requested user was not found'
+          });
+        }
+      });
+    } else{
+      callback(404,{
+        'error': 'Requested user was not found'
+      });
+    } 
+};
 handler._users.put = (requestProperties, callback) => {};
 handler._users.delete = (requestProperties, callback) => {};
 module.exports = handler;
